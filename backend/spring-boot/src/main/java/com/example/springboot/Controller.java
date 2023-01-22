e8c5e864a639cc2b3a1b8b6bdd7ed32aafec0d8d
chriswoojinlee <chrislee707@gmail.com>
2023-01-22 09:13:05 -0800

    cleaned up Controller

--- backend/spring-boot/src/main/java/com/example/springboot/Controller.java
+++ backend/spring-boot/src/main/java/com/example/springboot/Controller.java
@@ -6,17 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-	@GetMapping("/test")
-	public Object test(@RequestParam(value = "str", defaultValue = "No input!!") String str) throws Exception {
-		return str;
-	}
-
 	@GetMapping("/price")
 	public Object price(@RequestParam(value = "originCity", defaultValue = "Vancouver") String originCity
 		, @RequestParam(value = "originIATA", defaultValue = "YVR") String originIATA
-		, @RequestParam(value = "destinationCity", defaultValue = "Calgary") String destinationCity
-		, @RequestParam(value = "destinationIATA", defaultValue = "YYC") String destinationIATA
-		, @RequestParam(value = "departureDate", defaultValue = "2023-2-21") String departureDate
+		, @RequestParam(value = "destinationCity", defaultValue = "Incheon") String destinationCity
+		, @RequestParam(value = "destinationIATA", defaultValue = "ICN") String destinationIATA
+		, @RequestParam(value = "departureDate", defaultValue = "	") String departureDate
 		, @RequestParam(value = "returnDate", defaultValue = "2023-2-27") String returnDate
 		, @RequestParam(value = "cabinClass", defaultValue = "Economy") String cabinClass) throws Exception {
 		FlightPricing flightPricing = new FlightPricing(originCity, originIATA, destinationCity, destinationIATA, departureDate, returnDate, cabinClass);
