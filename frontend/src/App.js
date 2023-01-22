c6f6e2d5cd63aa6d4b9441e162d92133565b8972
Ameya <ameyagoel@gmail.com>
2023-01-21 21:03:46 -0800

    fixed bg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,31 +2,29 @@ import './App.css';
 import Searchbar from "./components/Searchbar";
 import Background from './components/Background';
 import bgImage from "./components/img/bgImage.png";
-import {Risk, Trip, Plane, RiskTitle} from './components/Images';
+import {Risk, Trip, Plane} from './components/Images';
 
 const style = {
   background: {backgroundImage: `url(${bgImage})`,
     backgroundRepeat: 'no-repeat',
+    backgroundsize:'cover',
+    overflow: 'hidden',
     width: window.innerWidth,
     height: window.innerHeight
-    
   },
 
 }
 
 function App() {
-  console.log(window.innerHeight);
-  console.log(window.innerWidth);
   return ( 
         
-        <div style={style.background}
-        >
-          <Trip style={style.trip}>
-            </Trip>
-          <Risk/>
+        <div style={style.background}>
+            <Trip/>
+            <Plane/>
+            <Risk/>
+            
+            {/* <Background/> */}
           
-          <Plane/>
-          <RiskTitle/>
           {/* <Searchbar/> */}
       </div>
     
